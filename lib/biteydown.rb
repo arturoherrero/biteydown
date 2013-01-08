78fc2c58f8910144e15e57db550a7f81e8b8bf0e
Arturo Herrero <arturo.herrero@gmail.com>
2013-01-08 02:27:43 +0100

    Create executable script to convert a markdown file to html and pdf files

--- lib/biteydown.rb
+++ lib/biteydown.rb
@@ -1,16 +1,52 @@
-#!/usr/bin/env ruby
-
 require 'github/markup'
 
-markdown_file = ARGV[0]
-output_dir = File.dirname(markdown_file) + '/'
-output_filename = File.basename(markdown_file, File.extname(markdown_file))
-html_file = "#{output_dir}#{output_filename}.html"
+class Biteydown
 
-if markdown_file && File.exists?(markdown_file)
-  File.open(html_file, 'w') do |f|
-    f.write(GitHub::Markup.render(markdown_file))
+  def self.process(markdown_file)
+    @converter = Converter.new
+
+    output_dir = File.dirname(markdown_file) + '/'
+    output_filename = File.basename(markdown_file, File.extname(markdown_file))
+    html_file = "#{output_dir}#{output_filename}.html"
+    pdf_file = "#{output_dir}#{output_filename}.pdf"
+
+    html = @converter.generate_html(markdown_file)
+
+    @converter.create_html_file(html, html_file)
+    @converter.create_pdf_file(html_file, pdf_file)
   end
-else
-  puts "usage: #{$0} FILE"
+
+end
+
+
+class Converter
+
+  def generate_html(markdown_file)
+    html = GitHub::Markup.render(markdown_file)
+    add_stylesheet!(html)
+    add_head!(html)
+  end
+
+  def add_head!(html)
+    html.insert(0, "\n<head>\n<meta charset='utf-8'>\n</head>\n")
+  end
+
+  def add_stylesheet!(html)
+    html.insert(0, "\n<style>#{File.read('style/style.css')}</style>\n")
+  end
+
+  def create_html_file(html, html_file)
+    File.open(html_file, 'w') do |f|
+      f.write(html)
+    end
+  end
+
+  def create_pdf_file(html_file, pdf_file)
+    `wkhtmltopdf #{html_file} \
+      --encoding UTF-8 \
+      --page-size Letter \
+      --quiet \
+      #{pdf_file}`
+  end
+
 end

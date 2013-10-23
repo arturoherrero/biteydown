e155672c1c94cefee34ca2a4faa64f06ce32c033
Lee Green <lee.x.green@gmail.com>
2013-10-23 10:36:12 -0400

    Allow path to CSS file to be passed as command line arg If not passed, default is vendored CSS

--- lib/biteydown.rb
+++ lib/biteydown.rb
@@ -4,12 +4,12 @@ require 'converter'
 
 class Biteydown
 
-  def self.process(markdown_path, generate_html, generate_pdf)
+  def self.process(markdown_path, generate_html, generate_pdf, css_path=nil)
     @markup = Markup.new
     @file_path = FilePath.new(markdown_path)
     @converter = Converter.new(@file_path)
 
-    css_path = File.expand_path('../../style/style.css', __FILE__)
+    css_path ||= File.expand_path('../../style/style.css', __FILE__)
     html = @markup.generate_html(markdown_path, css_path)
     @converter.create_pdf_file(html) if generate_pdf
     @converter.create_html_file(html) if generate_html

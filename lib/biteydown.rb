68fdacea3582d5cd654e96b40655117bf0b18164
Lee Green <lee.x.green@gmail.com>
2013-10-23 10:26:05 -0400

    Fix bug where biteydown only worked when run from biteydown/
    move knowledge about CSS path into Biteydown class
    make this path relative to lib/biteydown.rb

--- lib/biteydown.rb
+++ lib/biteydown.rb
@@ -9,7 +9,8 @@ class Biteydown
     @file_path = FilePath.new(markdown_path)
     @converter = Converter.new(@file_path)
 
-    html = @markup.generate_html(markdown_path)
+    css_path = File.expand_path('../../style/style.css', __FILE__)
+    html = @markup.generate_html(markdown_path, css_path)
     @converter.create_pdf_file(html) if generate_pdf
     @converter.create_html_file(html) if generate_html
   end

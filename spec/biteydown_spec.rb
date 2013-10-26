01b54b0e9f6c869f0a7a855c2c23a2da911ecc7d
Arturo Herrero <arturo.herrero@gmail.com>
2013-10-26 20:10:41 +0100

    Better specs

--- spec/biteydown_spec.rb
+++ spec/biteydown_spec.rb
@@ -1,23 +1,11 @@
 require 'biteydown'
 
 describe Biteydown do
-  markdown_path = 'example/curriculum.md'
-  html_path = 'example/curriculum.html'
-  pdf_path = 'example/curriculum.pdf'
-  vendor_css = 'style/style.css'
-  custom_css = 'style/empty.css'
-
-  it 'create html file from a markdown file' do
-    Biteydown.process(
-      :markdown_path => markdown_path,
-      :html          => true,
-      :pdf           => false,
-    )
- 
-    File.exists?(pdf_path).should be_false
-    File.exists?(html_path).should be_true
-    check_contains(html_path, vendor_css)
-  end
+  let(:markdown_path)   { 'example/curriculum.md' }
+  let(:html_path)       { 'example/curriculum.html' }
+  let(:pdf_path)        { 'example/curriculum.pdf' }
+  let(:vendor_css_path) { 'style/style.css' }
+  let(:custom_css_path) { 'style/empty.css' }
 
   it 'create pdf file from a markdown file' do
     Biteydown.process(
@@ -26,9 +14,22 @@ describe Biteydown do
       :pdf           => true,
     )
 
-    File.exists?(pdf_path).should be_true
-    File.exists?(html_path).should be_false
+    expect(File.exists?(pdf_path)).to be_true
+    expect(File.exists?(html_path)).to be_false
   end
+
+  it 'create html file from a markdown file' do
+    Biteydown.process(
+      :markdown_path => markdown_path,
+      :html          => true,
+      :pdf           => false,
+    )
+ 
+    expect(File.exists?(pdf_path)).to be_false
+    expect(File.exists?(html_path)).to be_true
+    check_css(html_path, vendor_css_path)
+  end
+
 
   it 'create html and pdf file from a markdown file' do
     Biteydown.process(
@@ -37,9 +38,9 @@ describe Biteydown do
       :pdf           => true,
     )
 
-    File.exists?(pdf_path).should be_true
-    File.exists?(html_path).should be_true
-    check_contains(html_path, vendor_css)
+    expect(File.exists?(pdf_path)).to be_true
+    expect(File.exists?(html_path)).to be_true
+    check_css(html_path, vendor_css_path)
   end
 
   it 'will include user-supplied CSS' do
@@ -47,12 +48,12 @@ describe Biteydown do
       :markdown_path => markdown_path,
       :html          => true,
       :pdf           => true,
-      :css_path      => custom_css,
+      :css_path      => custom_css_path,
     )
 
-    File.exists?(pdf_path).should be_true
-    File.exists?(html_path).should be_true
-    check_contains(html_path, custom_css)
+    expect(File.exists?(pdf_path)).to be_true
+    expect(File.exists?(html_path)).to be_true
+    check_css(html_path, custom_css_path)
   end
 
   after(:each) do
@@ -62,10 +63,11 @@ describe Biteydown do
 
   private
 
-  def check_contains(html_path, css_path)
+  def check_css(html_path, css_path)
     html_content = File.read(html_path)
     css_content = File.read(css_path)
     css_string = "<style>#{css_content}</style>"
-    html_content.include?(css_string).should be_true
+
+    expect(html_content.include?(css_string)).to be_true
   end
 end
